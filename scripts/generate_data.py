b5c7b82bad8ada022dd85f1eabf29b3754aab290
Lesego-Putswane <lesegoputswane@gmail.com>
2026-02-17 17:05:03 +0200

    Added transactions dataset in  generate_data.py and saved as transactions.csv

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -5,8 +5,10 @@ import random
 fake = Faker()
 
 customers = []
+transactions = []
+transactions_type = ['withdrawal', 'deposit', 'transfer', 'payment']
 
-for i in range(1, 101):  # 100 customers
+for i in range(1, 101):
     customers.append({
         "customer_id": i,
         "name": fake.name(),
@@ -15,7 +17,22 @@ for i in range(1, 101):  # 100 customers
         "credit_score": random.randint(300, 850)
     })
 
+for i in range(1, 1001):
+    transactions.append({
+        "transaction_id": i,
+        "customer_id": random.randint(1, 100),
+        "amount": random.randint(1, 10000),
+        "transaction_date": fake.date(),
+        "location": fake.city(),
+        "transaction_type": random.choice(transactions_type),
+    })
+
 customers_df = pd.DataFrame(customers)
 customers_df.to_csv("../data/customers.csv", index=False)
 
+transactions_df = pd.DataFrame(transactions)
+transactions_df.to_csv("../data/transactions.csv", index=False)
+
+
 print("Customers dataset created.")
+print("Transactions dataset created")
